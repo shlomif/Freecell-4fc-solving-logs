a14c47889871084d9ba76319e7a34a990d1bdf26
Shlomi Fish <shlomif@shlomifish.org>
2014-11-08 14:42:46 +0200

    add tfts to the summary.

--- summarize.pl
+++ summarize.pl
@@ -18,6 +18,16 @@ my $total = 900_000_000;
 
 my $solved_amateur_star = $total - $num_unsolved_amateur_star;
 
-print "Out of $total deals, Amateur Star:\n\t - solved $solved_amateur_star deals\n\t - left $num_unsolved_amateur_star as indetermined.\n\n";
+my $fh = io('-');
+$fh->print ( "Out of $total deals, Amateur Star:\n\t - solved $solved_amateur_star deals\n\t - left $num_unsolved_amateur_star as indetermined.\n\n" );
+
+my @tfts_solved = io('After-toons-for-twenty-somethings/solved.txt')->chomp->getlines;
+my @tfts_impossible = io('After-toons-for-twenty-somethings/unsolved.txt')->chomp->getlines;
+my @tfts_to_check = io('After-toons-for-twenty-somethings/to-check.txt')->chomp->getlines;
+
+my $tfts_solved = $solved_amateur_star + @tfts_solved;
+my $tfts_impossible = @tfts_impossible;
+
+$fh->print( "Toons-for-twenty-somethings deteremined:\n\t- solved $tfts_solved (total)\n\t- impossible $tfts_impossible\n\t- to check @{[scalar @tfts_to_check]}\n\n" );
 
 # print "@unsolved_amateur_star";
